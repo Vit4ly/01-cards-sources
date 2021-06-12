028b17b132a53796fe5c4445d93fe58d16a30d7e
Vitaliy <vit4ly.kiselev@gmail.com>
2021-06-12 16:54:49 +0300

    внес исправления в app.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(active = 0) {
+    const slides = document.querySelectorAll('.slide')
 
-for(const slide of slides) {
-    slide.addEventListener('click', () => {
-clearActiveClasses()
+    slides[active].classList.add('active')
 
-        slide.classList.add('active')
-    })
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    } )
-}
+slidesPlugin(2)
